1aa199e47489eebfe9f44201e3aa83f72e3affca
komronbekn <komronfront@gmail.com>
2024-04-15 15:52:34 +0500

    by komronbek

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let string = prompt('string typedagi malumot kiriting')
+let number = prompt('Number typedagi malumot kiritin')
+let boolean = prompt('boolean typedagi malumot kiriting')
+confirm('kiritilgan malumotlar toriligiga ishochingiz komilmi ?')
+alert(`string typedagi malumot ${string} 
+Number typedagi malumot ${number} 
+boolean typedagi malumot kiriting ${boolean}`)
