21b82ebfcc5c798de3c91850fb7077d8e9158af8
komronbekn <komronfront@gmail.com>
2024-04-18 23:42:14 +0500

    by komronbek

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
-let string = prompt('string typedagi malumot kiriting')
-let number = prompt('Number typedagi malumot kiritin')
-let boolean = prompt('boolean typedagi malumot kiriting')
+let string1 = prompt('string typedagi malumot kiriting')
+let number1 = +prompt('Number typedagi malumot kiritin')
+let boolean1 = prompt('boolean typedagi malumot kiriting')
+boolean1=Boolean
 confirm('kiritilgan malumotlar toriligiga ishochingiz komilmi ?')
-alert(`string typedagi malumot ${string} 
-Number typedagi malumot ${number} 
-boolean typedagi malumot kiriting ${boolean}`)
+alert(`siz kirtgan ${string1} sozi string typega tegishli
+siz kiritgan ${number1} sozi number type ga tegishli 
+siz kirtgan ${boolean1} boolean typega tegishli`)
+console.log(string1,typeof(string1) );
+console.log(number1, typeof(number1));
+console.log(boolean1, typeof(boolean1));
